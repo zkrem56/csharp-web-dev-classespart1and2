9e333e780b92b52da5a7b61ce6c6da26ec2b49d0
Carly Langlois <cj.langlois.cl@gmail.com>
2020-02-23 15:29:30 -0600

    starter code for lesson 3 exercises

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -1,50 +1,18 @@
 ﻿using System;
+
+// Start working here with your Student class.
+// To instantiate the Student class, add your code to the Main method in Program
+
 namespace SchoolPractice
 {
     public class Student
     {
-        private static int nextStudentId = 1;
         public string Name { get; set; }
         public int StudentId { get; set; }
-        public int NumberOfCredits { get; set; }
-        public double Gpa { get; set; }
+        public int NumberOfCredits { get; set; } = 0;
+        public double Gpa { get; set; } = 0.0;
 
-        public Student(string name, int studentId,
-            int numberOfCredits, double gpa)
-        {
-            Name = name;
-            StudentId = studentId;
-            NumberOfCredits = numberOfCredits;
-            Gpa = gpa;
-        }
-
-        public Student(string name, int studentId)
-        : this(name, studentId, 0, 0) { }
-
-        public Student(string name)
-        : this(name, nextStudentId)
-        {
-            nextStudentId++;
-        }
-
-        // TODO: Complete the AddGrade method.
-        public void AddGrade(int courseCredits, double grade)
-        {
-            // Update the appropriate properties: NumberOfCredits, Gpa
-        }
-
-        //TODO: Complete the GetGradeLevel method here:
-        public string GetGradeLevel(int credits)
-        {
-            // Determine the grade level of the student based on NumberOfCredits
-            return "grade level tbd";
-        }
-
-        // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
-        //  than just the class fields.
-
-        // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
-        //  Student objects equal.
+        
 
     }
 }
