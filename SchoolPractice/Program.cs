a0e02a5aa4cdafe572fa6e352bcd0120d57c3b43
Carly Langlois <cj.langlois.cl@gmail.com>
2020-02-23 01:35:59 -0600

    add starter code

---
+++ SchoolPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // TODO: Test your exercise solutions with print statements here.
+        }
+    }
+}
---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SchoolPractice
+{
+    public class Teacher
+    {
+        public string Topic { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public int YearsTeaching { get; set; }
+
+        public Teacher(string firstName, string lastName, string subject, int yearsTeaching)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Subject = subject;
+            YearsTeaching = yearsTeaching;
+        }
+
+    }
+}
